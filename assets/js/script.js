807c63778dba3fdc8907511426aa0fc5e1f40150
Clement Fonyuy <fonyuyclement@gmail.com>
2023-04-17 14:24:12 +0000

    add functionality to the login and signup buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,51 @@
-function login{
+let players = ['Kewel','Stephan'];
+
+let loginButton = document.getElementById('login-btn');
+loginButton.addEventListener('click', login);
+
+let signupButton = document.getElementById('signup-btn');
+signupButton.addEventListener('click', register);
+
+function login(event){
+    let username = document.getElementById('username');
+    let name = username.value;
+    if (players.includes(name)){
+        alert(`${name} is inn.`);
+        console.log(name);
+    }
+    else{
+        alert(`${name} does not exist. Pls signup.`);
+    }
+}
+
+function register(event) {
+    let newUser = document.getElementById('username');
+    let newPlayer = newUser.value;
+    if(players.includes(newPlayer)){
+        alert(`${newPlayer} already exist. Please login`)
+    }
+    else{
+        alert(`${newPlayer} registered.`)
+    }
+}
+
+function playGame(){
 
 }
 
-function playGame{
+function displayQuestions(){
 
 }
 
-function displayQuestions{
+function checkAnswer() {
 
 }
 
-function checkAnswer {
+function incrementCorrectAnswer(){
 
 }
 
-function incrementCorrectAnswer{
-
-}
-
-function increamentIncorrectAnswer{
+function increamentIncorrectAnswer(){
 
 }
 
